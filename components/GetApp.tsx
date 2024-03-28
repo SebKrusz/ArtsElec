1f52c3b5361493d9dab18d6a4cfbf935419537e6
alex.drouillard <alex.drouillard.9@hotmail.com>
2024-03-28 11:31:50 -0400

    Changes made to button, full property added

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Button from "./Button";
 
 const GetApp = () => {
 	return (
 		<section className="flexcenter w-full flex-col pb-[100px]">
-
+			<div className="get-app">
+				<div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
+					<h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get for free now!</h2>
+					<p className="regular-16 text-gray-10">Available on iOS and Android</p>
+					<div>
+						<Button />
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 };
